436425ebf5c83abbe2a48c80e19b9fe4d4b52619
Viet Pham <phpcodervn@gmail.com>
2016-12-17 23:51:52 +0700

    override theme only

--- traceability_themes/hooks.py
+++ traceability_themes/hooks.py
@@ -15,7 +15,7 @@ app_license = "MIT"
 # ------------------
 
 # include js, css files in header of desk.html
-# app_include_css = "/assets/traceability_themes/css/traceability_themes.css"
+app_include_css = "/assets/css/dark-theme.min.css"
 app_include_js = "/assets/js/traceability_themes.min.js"
 
 # Installation
